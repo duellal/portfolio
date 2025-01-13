7acc40389232c313b47fe186cea22444f7d92f5c
Alex <alexandriaduell@gmail.com>
2025-01-13 15:26:09 -0500

    Edited title and spacing between sections

--- src/components/title.js
+++ src/components/title.js
@@ -9,21 +9,17 @@ export default function Title(){
     return (
         <section id="title"> 
             <div className="name">
-                <p>
-                    Hi, my name is ...
-                </p>
                 <h1>
                     Alexandria Duell
                 </h1>
             </div>
+
             <div className="career">
-            <p>
-             I&apos;m a ...
-            </p>
-            <h2>
-                Software Engineer
-            </h2>
+                <h2>
+                    Software Engineer
+                </h2>
             </div>
+            
             <div className="sm-blurb">
                 <p>
                     I specialize in creating web applications and building databases. I enjoy projects that uses both my current knowledge, and challenges me to learn other technologies or ways of thinking.
