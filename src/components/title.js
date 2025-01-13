a11719d54e1240cb9987f1834696581d2494ebad
Alex <alexandriaduell@gmail.com>
2025-01-13 14:22:00 -0500

    Updated title blurb at the bottom + css related to it

--- src/components/title.js
+++ src/components/title.js
@@ -26,10 +26,7 @@ export default function Title(){
             </div>
             <div className="sm-blurb">
                 <p>
-                    I specialize in creating web applications and building databases. I enjoy projects that keep me on my toes. Currently, I am working at SchwaaWeb and updating the {' '}
-                    <a href="https://character-randomizer.netlify.app/" target="new">
-                        Character Randomization project.
-                    </a>
+                    I specialize in creating web applications and building databases. I enjoy projects that uses both my current knowledge, and challenges me to learn other technologies or ways of thinking.
                 </p>
             </div>
         </section>
