deb5b2d0453196f3d5364f0e9548a54478d42a89
Alex <alexandriaduell@gmail.com>
2026-05-15 16:07:17 -0400

    Updated title + contact sections; updated resume pdf

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,16 +4,17 @@ import React from "react"
 //Contact Styles:
 import "../styles/css/contact.css"
 
-export default function Contact(){
+export default function Contact() {
 
     return (
         <div id="contact">
-            <h3> Get in Touch with Alex! </h3>
+            <h3> Get in Touch </h3>
             <div>
                 <p>
                     Feel free to email me with any questions or job opportunities.
                 </p>
 
+                {/* Make this a contact page link */}
                 <div className="btn-div">
                     <button className="email-btn" onClick={() => { window.open(`mailto:aduellswe@gmail.com`) }}>
                         Contact Me
